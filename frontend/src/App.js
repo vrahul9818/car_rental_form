290d93037669825cba3eb9a45e1f2ce04ade9fff
vrahul9818 <vrahul9818@gmail.com>
2024-10-30 00:41:35 +0530

    Modified app.js,app.css and added all the FontImages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import '../src/App.css'; 
+import NameStep from './components/NameStep';
+import WheelsStep from './components/WheelsStep';
+import VehicleTypeStep from './components/VehicleTypeStep';
+import DateRangeStep from './components/DateRangeStep';
+import BookingHistoryStep from './components/BookingHistoryStep'; 
+import { submitBookingData } from './services/api';
 
-function App() {
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    wheels: '',
+    id: '',
+    dateRange: { startDate: '', endDate: '' }
+  });
+
+  const nextStep = () => setStep(step + 1);
+  const prevStep = () => setStep(step - 1);
+
+  const handleDataChange = (field, value) => {
+    setFormData({ ...formData, [field]: value });
+    console.log(formData);
+  };
+
+  const handleSubmit = async () => {
+    const confirmationMessage = `Booking For: ${formData.dateRange.startDate} to ${formData.dateRange.endDate}. Do you want to confirm?`;
+    
+    // Use window.confirm to ask for user confirmation
+    const userConfirmed = window.confirm(confirmationMessage);
+    
+    if (userConfirmed) {
+      try {
+        const returnData = await submitBookingData(formData); // Ensure to pass formData
+        if (returnData) {
+          alert("Your booking is submitted successfully!");
+          nextStep(); // Call your function to proceed to the next step
+        }
+      } catch (error) {
+        alert("There was an error submitting your booking. Please try again."); 
+        console.error("Error submitting booking:", error); 
+      }
+    } else {
+      console.log("Booking not confirmed. Submission canceled.");
+    }
+  };
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container"> 
+      {step === 1 && (
+        <NameStep nextStep={nextStep} handleDataChange={handleDataChange} formData={formData} />
+      )}
+      {step === 2 && (
+        <WheelsStep nextStep={nextStep} prevStep={prevStep} handleDataChange={handleDataChange} formData={formData} />
+      )}
+      {step === 3 && (
+                <DateRangeStep nextStep={nextStep} prevStep={prevStep} handleDataChange={handleDataChange} formData={formData} />
+
+      )}
+      {step === 4 && (
+                <VehicleTypeStep nextStep={nextStep}  handleSubmit={handleSubmit} prevStep={prevStep} handleDataChange={handleDataChange} formData={formData} />
+      )}
+      {step === 5 && ( 
+        <BookingHistoryStep handleSubmit={handleSubmit} prevStep={prevStep} handleDataChange={handleDataChange} formData={formData} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
