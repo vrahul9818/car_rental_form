f46411e2a349b9f0b8b727067fa4d52cf9087e54
vrahul9818 <vrahul9818@gmail.com>
2024-10-30 01:19:35 +0530

    added component folder for Name DateRange VehicleType and Bookings

---
+++ frontend/src/components/BookingHistoryStep.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { Typography, Button } from '@mui/material';
+import axios from 'axios';
+import '../components/css/NameStep.css';
+
+const BookingHistoryStep = ({ prevStep, formData }) => {
+  const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchBookingHistory = async () => {
+      try {
+        const response = await axios.get('http://localhost:5050/api/getAllBooking');
+        console.log(response);
+        setHistory(response.data);
+      } catch (err) {
+        setError('Failed to fetch booking history');
+        console.error('Error fetching booking history:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBookingHistory();
+  }, []);
+
+  if (loading) {
+    return <Typography>Loading booking history...</Typography>;
+  }
+
+  if (error) {
+    return <Typography color="error">{error}</Typography>;
+  }
+
+  return (
+    <div className="book-step-container">
+      <Typography variant="h4" gutterBottom>
+        All Booking History
+      </Typography>
+      {history.length > 0 ? (
+        <ul>
+          {history.map((booking, index) => (
+            <li key={index}>
+              {booking.firstName} {booking.lastName} - {booking.model} from {booking.startDate} to {booking.endDate} for     MODEL: {booking.vehicle['model']} TYPE: {booking.vehicle['type']}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <Typography>No booking history available.</Typography>
+      )}
+      <div className="button-container">
+        <Button variant="contained" color="primary" onClick={prevStep}>
+          Go Back
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default BookingHistoryStep;
---
+++ frontend/src/components/VehicleTypeStep.js
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Typography, Card, CardContent, CardMedia, FormControlLabel, Checkbox } from '@mui/material';
+import Cruser from '../FontImage/Cruser.png';
+import Hatchback from '../FontImage/Hatchback.png';
+import Sedan from '../FontImage/Sedan.png';
+import SUV from '../FontImage/SUV.png';
+import Sports from '../FontImage/Sports.png';
+import '../components/css/VehicleTypeStep.css'; 
+
+const VehicleTypeStep = ({ nextStep, handleSubmit,prevStep, handleDataChange, formData }) => {
+  const [error, setError] = useState('');
+  const [vehicleTypes, setVehicleTypes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedTypes, setSelectedTypes] = useState([]);
+
+  useEffect(() => {
+    console.log(formData);
+    const fetchVehicleTypes = async () => {
+      try {
+        const { wheels, dateRange } = formData;
+        const queryString = `wheels=${wheels}&startDate=${dateRange.startDate}&endDate=${dateRange.endDate}`;
+        console.log(queryString);
+        const response = await fetch(`http://localhost:5050/api/getvehicles?${queryString}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        setVehicleTypes(data);
+      } catch (error) {
+        setError('Failed to fetch vehicle types');
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    fetchVehicleTypes();
+  }, [formData.wheels,formData.startDate,formData.endDate]);
+  
+  const handleCardClick = (id) => {
+    console.log(id);
+    handleDataChange('id', id);
+    handleSubmit();
+  };
+
+  const vehicleOptions = formData.wheels === '2' ? ['Sports', 'Cruiser'] : ['SUV', 'Sedan', 'Hatchback'];
+
+  const handleCheckboxChange = (event) => {
+    const value = event.target.value;
+    setSelectedTypes(prevSelected =>
+      prevSelected.includes(value)
+        ? prevSelected.filter(type => type !== value)
+        : [...prevSelected, value]
+    );
+  };
+
+  const filteredVehicleTypes = selectedTypes.length > 0
+    ? vehicleTypes.filter(type => selectedTypes.includes(type.type))
+    : vehicleTypes;
+
+  const vehicleImages = {
+    Cruiser: Cruser,
+    Hatchback: Hatchback,
+    Sedan: Sedan,
+    SUV: SUV,
+    Sports: Sports,
+  };
+
+  return (
+    <div className="vehicle-type-container">
+      <Box>
+        <Typography variant="h4" gutterBottom>
+          Select Vehicle Type
+        </Typography>
+
+        <div className="vehicle-type-checkbox-container">
+          {vehicleOptions.map((option, index) => (
+            <FormControlLabel
+              key={index}
+              control={
+                <Checkbox
+                  checked={selectedTypes.includes(option)}
+                  onChange={handleCheckboxChange}
+                  value={option}
+                />
+              }
+              label={option}
+            />
+          ))}
+        </div>
+
+        {loading ? (
+          <Typography>Loading vehicle types...</Typography>
+        ) : (
+          <div className="vehicle-type-grid">
+            {filteredVehicleTypes.length > 0 ? (
+              filteredVehicleTypes.map((type) => (
+                <Card
+                  key={type.id}
+                  className="vehicle-type-card"
+                  onClick={() => handleCardClick(type.id)}
+                >
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={vehicleImages[type.type] || 'path/to/fallback/image.png'} // Use fallback if needed
+                    alt={type.model}
+                    className="vehicle-type-card-img"
+                  />
+                  <CardContent sx={{ textAlign: 'center' }}>
+                    <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                      {type.model}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Type: {type.type}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Wheels: {type.wheels}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              ))
+            ) : (
+              <Typography>No vehicle types available for the selected category.</Typography>
+            )}
+          </div>
+        )}
+
+        {error && <Typography color="error">{error}</Typography>}
+      </Box>
+
+      <Button variant="contained" color="secondary" onClick={prevStep} fullWidth>
+        Back
+      </Button>
+    </div>
+  );
+};
+
+export default VehicleTypeStep;
