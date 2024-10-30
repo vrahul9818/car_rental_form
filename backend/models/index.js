04a52355268e80106103e360217a98aa94d12a49
vrahul9818 <122368342+vrahul9818@users.noreply.github.com>
2024-10-30 13:59:48 +0530

    Update index.js in Models to use hosted database on PHP my admin

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 let env_arr = ['production','development']
-const env = env_arr[1];
+const env = env_arr[0];
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 // console.log(config);
